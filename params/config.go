fc15d007f279eb116674c6606769e83bd35d9074
Codegita <codegita@valorbit.com>
2020-03-21 20:41:30 -0700

    genesis hash

--- params/config.go
+++ params/config.go
@@ -27,12 +27,12 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash    = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
-	TestnetGenesisHash    = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
-	RinkebyGenesisHash    = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
-	GoerliGenesisHash     = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
-	ValorbitGenesisHash = common.HexToHash("0xe9884b54854853cec49611bba74ff91fef3fec42356c2501bf3046e6ee411a27")
-	GranvilleGenesisHash  = common.HexToHash("0x1966030e1e03b1106c081146400ec947950ab8d867a79c0ff27a31e661dd9282")
+	MainnetGenesisHash   = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
+	TestnetGenesisHash   = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
+	RinkebyGenesisHash   = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
+	GoerliGenesisHash    = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
+	ValorbitGenesisHash  = common.HexToHash("0xc5d09350b55cbe5e76f8c167f8a922a70d64c2350110634c05ee6df2ddd8b501")
+	GranvilleGenesisHash = common.HexToHash("0xc6b3ace065cee74752d30ff8a67a7627e1eab319bdf8b8d8895cf47fca0c4099")
 )
 
 // TrustedCheckpoints associates each known checkpoint with the genesis hash of
